62174a14f0037a9200c31567c24da6012741acc6
Jinhjlog <dndn2615@gmail.com>
2025-08-13 20:06:41 +0900

    ✨ screen: PostScreen 구현 - Post 목록 표시 및 상태관리 연동

--- app/screens/PostScreen.tsx
+++ app/screens/PostScreen.tsx
@@ -1,15 +1,25 @@
-import { FC } from "react"
+import { FC, useEffect } from "react"
 import { ViewStyle } from "react-native"
 
 import { Screen } from "@/components/Screen"
 import { Text } from "@/components/Text"
 import type { AppStackScreenProps } from "@/navigators/AppNavigator"
-
-// import { useNavigation } from "@react-navigation/native"
+import { usePostStore } from "@/stores"
 
 interface PostScreenProps extends AppStackScreenProps<"Post"> {}
 
 export const PostScreen: FC<PostScreenProps> = () => {
+  const { posts, fetchPosts } = usePostStore()
+
+  useEffect(() => {
+    console.log("Fetching posts...")
+    fetchPosts()
+  }, [])
+
+  useEffect(() => {
+    console.log(Object.values(posts))
+  }, [posts])
+
   // Pull in navigation via hook
   // const navigation = useNavigation()
   return (
