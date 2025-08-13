2ba8ee3da457458d94bba5182b21d7c2f2928449
Jinhjlog <dndn2615@gmail.com>
2025-08-13 20:06:19 +0900

    ✨ domain: Post 도메인 모델 완성 - getter 메서드 및 불변성 보장

--- app/domain/models/Post.ts
+++ app/domain/models/Post.ts
@@ -1,15 +1,45 @@
 export interface PostProps {
-  id: string
-  title: string
-  body: string
-  tags: string[]
-  reactions: { likes: number; dislikes: number }
-  views: number
-  userId: number
+  _id: string
+  _title: string
+  _body: string
+  _tags: string[]
+  _reactions: { _likes: number; _dislikes: number }
+  _views: number
+  _userId: number
 }
 
-export class Post {
+export class Post implements PostProps {
+  _id!: string
+  _title!: string
+  _body!: string
+  _tags!: string[]
+  _reactions!: { _likes: number; _dislikes: number }
+  _views!: number
+  _userId!: number
+
   constructor(props: PostProps) {
     Object.assign(this, props)
+  }
+
+  get id(): string {
+    return this._id
+  }
+  get title(): string {
+    return this._title
+  }
+  get body(): string {
+    return this._body
+  }
+  get tags(): string[] {
+    return [...this._tags]
+  }
+  get reactions(): { _likes: number; _dislikes: number } {
+    return { ...this._reactions }
+  }
+  get views(): number {
+    return this._views
+  }
+  get userId(): number {
+    return this._userId
   }
 }
